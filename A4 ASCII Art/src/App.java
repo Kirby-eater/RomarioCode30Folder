e3a32e42e53511f99c82db1f33220983a9bd9f58
Kirby-eater <Romario.bonnick@rcsd.ca>
2024-09-16 08:53:36 -0600

    Friday

---
+++ A4 ASCII Art/src/App.java
@@ -0,0 +1,11 @@
+// Project Name: ASCII Art
+// Name: Romario Bonnick
+// Date: 16 Sept 2024
+// Description: text art
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
