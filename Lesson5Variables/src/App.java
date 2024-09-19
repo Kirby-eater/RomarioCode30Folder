887b0e0e9c3866e47dab64a96330d3a4592de46f
Kirby-eater <Romario.bonnick@rcsd.ca>
2024-09-19 09:48:12 -0600

    End of Sept. 19

---
+++ Lesson5Variables/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String name = new String("Fox");
+        //String name = "Fox";
+
+        System.out.println(name == "Fox");
+        System.out.println(name.equals"Fox");
+    }
+}
