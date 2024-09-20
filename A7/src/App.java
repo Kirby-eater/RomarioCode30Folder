5aaa25bd90c2702da20c7578de8eea3c517b5528
Kirby-eater <Romario.bonnick@rcsd.ca>
2024-09-20 09:35:09 -0600

    working on a7

---
+++ A7/src/App.java
@@ -0,0 +1,17 @@
+// Project Name: String Basics
+// Name: Romario Bonnick
+// Date: 20 September 2024
+// Description: coding to remove and change words in code
+
+
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        String message1 = "I am very happy!";
+        String target1 = "very";
+
+        String message2 = "That was great - lol.";
+        String target2 = "lol";
+    }
+}
