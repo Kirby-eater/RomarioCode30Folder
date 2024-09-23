438ba5d59710a7fc527484997e15acaa4b49b8c4
Kirby-eater <Romario.bonnick@rcsd.ca>
2024-09-23 09:26:08 -0600

    Completed A7

--- A7/src/App.java
+++ A7/src/App.java
@@ -10,8 +10,17 @@ public class App {
     public static void main(String[] args) throws Exception {
         String message1 = "I am very happy!";
         String target1 = "very";
+        int length1 = target1.length();
+        int one = message1.indexOf(target1);
+        System.out.println(message1.replace("very","" ));
+        
+
 
         String message2 = "That was great - lol.";
         String target2 = "lol";
+        int length2 = target2.length();
+        int two = message2.indexOf(target2);
+        System.out.println(message2.replace("lol","laugh out loud"));
+
     }
 }
