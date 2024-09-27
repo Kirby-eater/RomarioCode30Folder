b188dba17c347419d3255cbba8a436b92b213bcb
Kirby-eater <Romario.bonnick@rcsd.ca>
2024-09-27 09:48:30 -0600

    finished A8b

---
+++ A8b/src/Optimus.java
@@ -0,0 +1,10 @@
+public class Optimus {
+    public void speak() {
+        System.out.println("I am Optimus Prime");
+    }
+
+    public void sayName() {
+        System.out.println("All for one, One for All");
+        speak();
+    }
+}
---
+++ A8b/src/OptimusRunner.java
@@ -0,0 +1,14 @@
+// Project Name: Passing methods
+// Name: Romario Bonnick
+// Date: 27 September 2024
+// Description: Create a sub class with two methods create a runner class that calls those methods and shows an output.
+
+public class OptimusRunner {
+    public static void main(String[] args) throws Exception {
+        Optimus autobot = new Optimus();
+
+        autobot.speak();
+        autobot.sayName();
+        autobot.speak();
+    }
+}
