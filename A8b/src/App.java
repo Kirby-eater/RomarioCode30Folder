09ef0f69292efa1877e372ffd38454ae1be9923a
Kirby-eater <Romario.bonnick@rcsd.ca>
2024-09-27 09:38:04 -0600

    finished demo

--- A8b/src/App.java
+++ A8b/src/App.java
@@ -1,24 +1,5 @@
-// Project Name: String Basics
-// Name: Romario Bonnick
-// Date: 26 September 2024
-// Description: Create a sub class with two methods
-
-
-public class App
-{
-    public void speak()
-    {
-    System.out.println("woof-woof");
-}
-}
-
-public void sayName()
-{
-    System.out.println("big dog");
-    speak();
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
     }
-
-
-Dog fun = new Dog();
-dog.speak();
-dog.sayName();
+}
