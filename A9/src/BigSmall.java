266e88e81613f6b60ac3f42b8ec8cf83b8631a0f
Kirby-eater <Romario.bonnick@rcsd.ca>
2024-10-02 09:48:31 -0600

    Started A9

---
+++ A9/src/BigSmall.java
@@ -0,0 +1,13 @@
+public class BigSmall {
+
+    private int num1 = 0;
+    private int num2 = 0;
+    
+    public void input() {
+
+    }
+
+    public void check() {
+
+    }
+}
---
+++ A9/src/BigSmallRunner.java
@@ -0,0 +1,10 @@
+// Project Name: Big or Small
+// Name: Romario Bonnick
+// Date: 2 October 2024
+//  Write a program to check 2 parameters.  If a is > b, output "yes".  If b is > a, output "no".  If a is equal to b, return "equals".
+
+public class BigSmallRunner {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
