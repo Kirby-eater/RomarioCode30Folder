948c0341b2b2d4864c51a33cb1f2111ffeb08c75
Kirby-eater <Romario.bonnick@rcsd.ca>
2024-10-09 09:49:03 -0600

    working on A12

---
+++ A12/src/Fahrenheit.java
@@ -0,0 +1,15 @@
+//import java.util.Scanner;
+public class Fahrenheit {
+    
+    public void check() {
+
+        System.out.print();
+    }
+
+// Fahrenheit to Celsius
+    public void solve() {
+
+       double fah = ((98.6 - 32) * 5/9);
+       System.out.print(fah);
+    }
+}
---
+++ A12/src/FahrenheitRunner.java
@@ -0,0 +1,12 @@
+// Project Name: Fahrenheit to Celsius
+// Name: Romario Bonnick
+// Date: 9 October 2024
+// Description: Convert Fahrenheit to Celsius
+
+public class FahrenheitRunner {
+    public static void main(String[] args) throws Exception {
+        Fahrenheit inte = new Fahrenheit();
+
+        inte.solve();
+    }
+}
