65044361e4b26946f16dc2210206f9e3cbad3745
Kirby-eater <Romario.bonnick@rcsd.ca>
2024-10-11 09:49:37 -0600

    Friday

---
+++ A13/src/App.java
@@ -0,0 +1,23 @@
+// Project Name: Geometry Calculator
+// Name: Romario Bonnick
+// Date: 10 October 2024
+// Description: 
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        App calc = new App();
+        
+        System.out.print("Perimeter of a Rectangle = a:");
+        System.out.println("Surface area of a cube = b:");
+        System.out.println("Area of a Circle = c:");
+        System.out.println("What would you like to do (a/b/c)?");
+
+        If 
+
+
+
+    
+    
+    
+    }
+}
---
+++ A13/src/Submethod.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Submethod {
+   private Scanner keys = new Scanner(System.in);
+    private double ans =0;
+    private double len =0;
+    private double wid =0;
+    private double sid =0;
+    private double rad =0;
+    public String choice ="";
+
+
+
+
+    public void Perimeter() {
+         ans = (2*len) + (2*wid); 
+
+
+    }
+
+    public void SurfaceArea() {
+         ans = 6s2
+
+
+    }
+
+
+    public void AreaCircle() {
+         ans = Math.PI * (rad * rad);
+
+
+    }
+
+}
