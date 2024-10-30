df165a2e884495a0c7ffbd058775ae81e9bfbf64
Kirby-eater <Romario.bonnick@rcsd.ca>
2024-10-30 09:48:32 -0600

    Wednesday

---
+++ A23/src/CoolNums.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class CoolNums {
+    private Scanner keys = new Scanner(System.in);
+    private int c1 = 0;
+    private int c2 = 0;
+    private int c3 = 0;
+    private int c4 = 0;
+    private int user = 0;
+
+    public void check() {
+        c1 = user % 3;
+        c2 = user % 4;
+        c3 = user % 5;
+        c4 = user % 6;
+
+        if(c1 == 1 || c2 == 1 || c3 == 1 || c4 ==1);
+        
+    }
+
+}
---
+++ A23/src/CoolNumsRunner.java
@@ -0,0 +1,10 @@
+// Project Name: Cool Numbers
+// Name: Romario Bonnick
+// Date: 30 October 2024
+// Description: Determine if a number is a cool number
+
+public class CoolNumsRunner {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
