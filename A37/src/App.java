7adfca3a25666734988f0771e7826c78e49bebe3
Kirby-eater <Romario.bonnick@rcsd.ca>
2024-12-11 09:48:27 -0600

    Wednesday

---
+++ A37/src/App.java
@@ -0,0 +1,12 @@
+// Name: Romario Bonnick
+// Project Name: Reverse Raffle Draw
+// Date: 11 December 2024
+// Description: A raffle draw where the name drawn from a hat they DOES NOT win. The last name drawn is the actual winner.
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("In this game, there are names in a hat and you want your name to be drawn out last to win the game");
+        Thread.sleep(1600);
+        System.out.println("Good Luck!");
+    }
+}
---
+++ A37/src/Raffle.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Raffle {
+    
+    public void Mixer() {
+    Scanner keys = new Scanner(System.in);
+
+    //String[] names = null;
+    String user = keys.next();
+
+    
+    //names = new String[12];
+
+    String[] names = {"Wolf", "Sutter", "Cook", "Fox", "Golden"};
+
+}
+
+}
